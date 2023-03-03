d4d8d7bcc65b87e7f8a5e091e3553d5e61e9d928
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-03-03 18:53:22 -0500

    Add custom student route

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,32 @@ import Layout from "./components/Layout";
 import Students from "./Pages/Students";
 import Subjects from "./Pages/Subjects";
 import Student from "./Pages/Student";
-import Teachers from "./Pages/Teachers";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Me from "./Pages/Me";
+import { AuthProvider } from "./Context/student";
+import Login from "./Pages/Login";
 
 function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<General />} />
-        <Route path="/students" element={<Students />} />
-        <Route path="/students/:id" element={<Student />} />
-        <Route path="/subjects" element={<Subjects />} />
-      </Route>
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<General />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/students" element={<Students />} />
+          <Route path="/students/:id" element={<Student />} />
+          <Route path="/subjects" element={<Subjects />} />
+          <Route
+            path="/me"
+            element={
+              <ProtectedRoute>
+                <Me />
+              </ProtectedRoute>
+            }
+          />
+        </Route>
+      </Routes>
+    </AuthProvider>
   );
 }
 
