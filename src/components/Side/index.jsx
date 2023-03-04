b86aab47b8050a66ef2e7c58e2047ff4e4090095
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-03-03 21:22:30 -0500

    Add sign out

--- src/components/Side/index.jsx
+++ src/components/Side/index.jsx
@@ -3,14 +3,16 @@ import SideMenuElement from "./SideMenuElement";
 import PeopleIcon from "../Icons/People";
 import PersonIcon from "../Icons/Person";
 import BooksIcon from "../Icons/Books";
-import TeachersIcons from "../Icons/Teachers";
 import SideLogo from "./SideLogo";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import HomeIcon from "../Icons/Home";
+import useAuth from "../../hooks/useAuth";
 
 export default function SideMenu() {
   const { pathname } = useLocation();
+  const navigate = useNavigate();
   const urlPattern = /(\/)students(\/)[0-9]+$/gm;
+  const { user, signOut } = useAuth();
 
   return (
     <div className="flex h-screen  flex-col justify-between border-r bg-white">
@@ -51,27 +53,30 @@ export default function SideMenu() {
         </nav>
       </div>
 
-      {/* <div className="sticky inset-x-0 bottom-0 border-t border-gray-100">
-        <a
-          href="#"
-          className="flex items-center gap-2 bg-white p-4 hover:bg-gray-50"
-        >
-          <img
-            alt="Man"
-            src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-            className="h-10 w-10 rounded-full object-cover"
-          />
-
-          <div>
-            <p className="text-xs">
-              <strong className="block font-medium">Eric Frusciante</strong>
-
-              <span> eric@frusciante.com </span>
-            </p>
-          </div>
-        </a>
-      </div> */}
-      {/* Activo cuando ingresa */}
+      {user && (
+        <div className="sticky inset-x-0 bottom-0 border-t border-gray-100">
+          <Link
+            to="/me"
+            className="flex items-center gap-2 bg-white p-4 hover:bg-gray-50"
+          >
+            <div>
+              <p className="text-xs">
+                <strong className="block font-medium">ESTÁS ADENTRO</strong>
+              </p>
+              <button
+                onClick={() =>
+                  signOut(() => {
+                    navigate("/login");
+                  })
+                }
+                className="rounded bg-indigo-600 px-8 py-1 text-sm font-medium text-white transition hover:bg-indigo-700"
+              >
+                Cerrar Matricula
+              </button>
+            </div>
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
