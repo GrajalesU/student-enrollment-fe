78e631f5e49a92c63da69a3b6d626b13920ea1e3
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-03-03 21:23:21 -0500

    Add data layout handler

--- src/Pages/Subjects/index.jsx
+++ src/Pages/Subjects/index.jsx
@@ -3,29 +3,31 @@ import SubjectElement from "../../components/SubjectElement";
 import useFetch from "../../hooks/useFetch";
 
 export default function Subjects() {
-  const { data, error, loading } = useFetch("http://localhost:5000/subjects");
+  const { data } = useFetch("http://localhost:5000/subjects");
   const subjects = data?.rows;
-  return (
-    <section>
-      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
-        <header className="mb-20">
-          <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">
-            Materias
-          </h2>
 
-          <p className="mt-4 max-w-md text-gray-500">
-            Estas son las materias que tenemos ofertadas para este semestre,
-            esperamos que las aproveches lo máximo posible y que hagas de este
-            semestre una experiencia inolvidable.
-          </p>
-        </header>
+  if (data)
+    return (
+      <section>
+        <div className="mx-auto max-w-screen-2xl w-full px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+          <header className="mb-20">
+            <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">
+              Materias
+            </h2>
 
-        <ul className="mt-4 mx-auto grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
-          {subjects?.map((subject) => (
-            <SubjectElement {...subject} key={subject.id} />
-          ))}
-        </ul>
-      </div>
-    </section>
-  );
+            <p className="mt-4 max-w-md text-gray-500">
+              Estas son las materias que tenemos ofertadas para este semestre,
+              esperamos que las aproveches lo máximo posible y que hagas de este
+              semestre una experiencia inolvidable.
+            </p>
+          </header>
+
+          <ul className="mt-4 mx-auto grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
+            {subjects?.map((subject) => (
+              <SubjectElement {...subject} key={subject.id} />
+            ))}
+          </ul>
+        </div>
+      </section>
+    );
 }
